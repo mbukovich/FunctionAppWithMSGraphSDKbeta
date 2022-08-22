16238b6124eef8906442c76e9bc9658311fae6a3
mark.bukovich46@gmail.com <mark.bukovich46@gmail.com>
2022-08-22 17:25:19 -0500

    Updated the app to use a custom TokenCredential class
    I wrote a custom TokenCredential class that uses the
    ROPC flow with a client secret.

--- FunctionAppWithMSGraphSDKbeta/Function1.cs
+++ FunctionAppWithMSGraphSDKbeta/Function1.cs
@@ -9,6 +9,10 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Azure.Identity;
 using Microsoft.Graph;
+using Azure.Core;
+using System.Threading;
+using System.Net.Http;
+using System.Collections.Generic;
 
 namespace FunctionAppWithMSGraphSDKbeta
 {
@@ -26,18 +30,18 @@ namespace FunctionAppWithMSGraphSDKbeta
              */
 
 
-            // For this sample to work, your application should have the application permissions:
+            // For this sample to work, your application should have the delegated permissions added and consented by an admin:
             //  "Directory.Read.All"
             //  "AuditLog.Read.All"
 
 
-            // The client credentials flow requires that you request the
-            // /.default scope, and preconfigure your permissions on the
+            // The Username-Password flow requires that you request the previously
+            // consented scopes, and preconfigure your permissions on the
             // app registration in Azure. An administrator must grant consent
             // to those permissions beforehand.
-            var scopes = new[] { "https://graph.microsoft.com/.default" };
+            var scopes = new[] { "AuditLog.Read.All", "Directory.ReadWrite.All" };
 
-            // Multi-tenant apps can use "common",
+
             // single-tenant apps must use the tenant ID from the Azure portal
             var tenantId = "{put your tenant id here}";
 
@@ -45,17 +49,15 @@ namespace FunctionAppWithMSGraphSDKbeta
             var clientId = "{put the client id for your application here}";
             var clientSecret = "{put your application's client secret here}";
 
-            // using Azure.Identity;
-            var options = new TokenCredentialOptions
-            {
-                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
-            };
+            var username = "{replace with the appropriate username}";
+            var password = "{replace with the password}";
 
-            // https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
-            var clientSecretCredential = new ClientSecretCredential(
-                tenantId, clientId, clientSecret, options);
 
-            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+            // Use our custom TokenCredential class: ROPCConfidentialTokenCredential
+            var ROPCCredential = new ROPCConfidentialTokenCredential(username, password, tenantId, clientId, clientSecret);
+
+
+            var graphClient = new GraphServiceClient(ROPCCredential, scopes);
 
             var users = await graphClient.Users.Request().Select("signInActivity").GetAsync();
 
@@ -69,6 +71,80 @@ namespace FunctionAppWithMSGraphSDKbeta
             string responseMessage = firstSignInDateTime;
 
             return new OkObjectResult(responseMessage);
+        }
+    }
+
+    public class ROPCConfidentialTokenCredential : Azure.Core.TokenCredential
+    {
+        // Implementation of the Azure.Core.TokenCredential class
+        string _username = "";
+        string _password = "";
+        string _tenantId = "";
+        string _clientId = "";
+        string _clientSecret = "";
+
+        string _tokenEndpoint = "";
+
+        public ROPCConfidentialTokenCredential(string username, string password, string tenantId, string clientId, string clientSecret)
+        {
+            // Public Constructor
+            _username = username;
+            _password = password;
+            _tenantId = tenantId;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+
+            _tokenEndpoint = "https://login.microsoftonline.com/" + _tenantId + "/oauth2/v2.0/token";
+        }
+
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            // Create the request body
+            var Parameters = new List<KeyValuePair<string, string>> 
+            {
+                new KeyValuePair<string, string>("client_id", _clientId),
+                new KeyValuePair<string, string>("client_secret", _clientSecret),
+                new KeyValuePair<string, string>("scope", string.Join(" ", requestContext.Scopes)),
+                new KeyValuePair<string, string>("username", _username),
+                new KeyValuePair<string, string>("password", _password),
+                new KeyValuePair<string, string>("grant_type", "password")
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, _tokenEndpoint) 
+            {
+                Content = new FormUrlEncodedContent(Parameters)
+            };
+            var response = httpClient.SendAsync(request).Result.Content.ReadAsStringAsync().Result;
+            dynamic responseJson = JsonConvert.DeserializeObject(response);
+            var expirationDate = DateTimeOffset.UtcNow.AddMinutes(60.0);
+            return new AccessToken(responseJson.access_token.ToString(), expirationDate);
+        }
+
+        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            // Create the request body
+            var Parameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("client_id", _clientId),
+                new KeyValuePair<string, string>("client_secret", _clientSecret),
+                new KeyValuePair<string, string>("scope", string.Join(" ", requestContext.Scopes)),
+                new KeyValuePair<string, string>("username", _username),
+                new KeyValuePair<string, string>("password", _password),
+                new KeyValuePair<string, string>("grant_type", "password")
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, _tokenEndpoint)
+            {
+                Content = new FormUrlEncodedContent(Parameters)
+            };
+            var response = httpClient.SendAsync(request).Result.Content.ReadAsStringAsync().Result;
+            dynamic responseJson = JsonConvert.DeserializeObject(response);
+            var expirationDate = DateTimeOffset.UtcNow.AddMinutes(60.0);
+            return new ValueTask<AccessToken>(new AccessToken(responseJson.access_token.ToString(), expirationDate));
         }
     }
 }
